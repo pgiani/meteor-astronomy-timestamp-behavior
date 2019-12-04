f8ca181ab869a61853587c07325456be0be10da2
pablo Giani <pgiani@gmail.com>
2019-12-04 13:12:41 -0700

    updated package info

---
+++ package.js
@@ -0,0 +1,17 @@
+Package.describe({
+  name: 'pgiani:astronomy-timestamp-behavior',
+  version: '2.0.0',
+  summary: 'redis-oplog compatible timestamp behavior for Meteor Astronomy',
+  git: 'https://github.com/pgiani/Soft-remove-behavior-for-Meteor-Astronomy',
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.3');
+
+  api.use(
+    ['ecmascript', 'es5-shim', 'pgiani:redis-astronomy@1.0.0'],
+    ['client', 'server'],
+  );
+
+  api.mainModule('lib/main.js', ['client', 'server']);
+});
